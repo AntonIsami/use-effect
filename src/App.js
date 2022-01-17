2eb8092435314e48e3ef12862874a259fb744257
Antoni <antoniibrahimdev@gmail.com>
2022-01-17 12:04:02 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const apiKey = 'k2MyQP4T'; 
+
+  const [ images, setImages ] = useState([]);
+  const [ userInput, setUserInput ] = useState("");
+  const [ searchTerm, setSearchTerm ] = useState("");
+
+  useEffect(()=>{
+    axios({
+      url: 'https://www.rijksmuseum.nl/api/en/collection',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        key: apiKey,
+        imgonly: true,
+        q: searchTerm
+      }
+    }).then((response) => {
+      setImages(response.data.artObjects);
+    });
+  },[searchTerm])
+
+  const handleInput = (event) => {
+    setUserInput(event.target.value);
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(userInput);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome back to the art Museum</h1>
+
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor='search'>Search for Art</label>
+        <input type="text" id="search" 
+        onChange={ handleInput } value={userInput}/>
+        <button>Search</button>
+      </form>
+      {
+        images.map((artwork) => {
+          
+          return (
+            <div key={artwork.id}>
+              
+              <h2>{artwork.longTitle}</h2>
+              <img src={artwork.webImage.url} alt={artwork.title}/>
+              
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
